f0a7f379aa5a1d771bd9a9df53a60fb2fd395488
martin <durcansky.martin@gmail.com>
2019-07-13 15:34:55 +0200

    sprava2

--- src/arrays/sort/Triedenie.java
+++ src/arrays/sort/Triedenie.java
@@ -8,11 +8,14 @@ public class Triedenie {
 
         System.out.println("Hello World!");
         // Get the Array
-        int intArr[] = { 10, 20, 15, 22, 35 };
+        int intArr[] = { 10, 20, 15, 22, 35,1 };
 
         // To convert the elements as List
         System.out.println("Integer Array as List: "
                 + Arrays.asList(intArr));
+
+        //sortovanie
+        Arrays.sort(intArr);
 
         System.out.println("Integer Array: "
                 + Arrays.toString(intArr));
