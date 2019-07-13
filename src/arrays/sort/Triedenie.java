4c917101da8a22ec7ae0bb189514b5ebcf7786c5
martin <durcansky.martin@gmail.com>
2019-07-13 16:27:30 +0200

    sprava2

--- src/arrays/sort/Triedenie.java
+++ src/arrays/sort/Triedenie.java
@@ -1,5 +1,6 @@
 package arrays.sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Triedenie {
@@ -8,16 +9,38 @@ public class Triedenie {
 
         System.out.println("Hello World!");
         // Get the Array
-        int intArr[] = { 10, 20, 15, 22, 35,1 };
+        int pole[] = { 10, 20, 15, 22, 35,1 };
 
-        // To convert the elements as List
+        System.out.println(Arrays.toString(pole));
+
+        Arrays.sort(pole);
+
+        System.out.println(Arrays.toString(pole));
+
+       // ArrayList list = (ArrayList) Arrays.asList(pole);
+;
+ /*
         System.out.println("Integer Array as List: "
                 + Arrays.asList(intArr));
 
-        //sortovanie
+
         Arrays.sort(intArr);
 
         System.out.println("Integer Array: "
                 + Arrays.toString(intArr));
+
+
+        int intArr1[] = { 10, 15, 22 };
+
+        if (Arrays.equals(intArr, intArr1))
+            System.out.println("Same");
+        else
+            System.out.println("Not same");
+*/
+
+        // To compare both arrays
+        // To compare both arrays
+
+
     }
 }
